2b8ccc781da920ded2b3b324f295c2d6ef575f72
gktgkrnlk <goktug.karanlik@ogr.dpu.edu.tr>
2024-02-23 22:22:08 +0300

    1st

--- Notter/ViewController.swift
+++ Notter/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var textView: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Eğer varsa kayıtlı notu yükle
+        if let savedNote = UserDefaults.standard.string(forKey: "savedNote") {
+            textView.text = savedNote
+        }
+        
+        
     }
-
-
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        // Notu kaydet
+        UserDefaults.standard.set(textView.text, forKey: "savedNote")
+                showAlert(message: "Not kaydedildi.")
+            }
+            
+            func showAlert(message: String) {
+                let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Tamam", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
-
